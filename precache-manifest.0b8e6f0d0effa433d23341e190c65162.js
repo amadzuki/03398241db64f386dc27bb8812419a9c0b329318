ee0e74ba33ed4117f9306cba1b230c9eb49530ff
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-09-26 19:46:46 +0700

    Updates

---
+++ precache-manifest.0b8e6f0d0effa433d23341e190c65162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630558455786dac483307da277ab72ee",
+    "url": "03398241db64f386dc27bb8812419a9c0b329318/index.html"
+  },
+  {
+    "revision": "b2cdd9e56f42c80150f5",
+    "url": "03398241db64f386dc27bb8812419a9c0b329318/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c7a700ca22b4cb5d955a",
+    "url": "03398241db64f386dc27bb8812419a9c0b329318/static/js/2.a835d7da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "03398241db64f386dc27bb8812419a9c0b329318/static/js/2.a835d7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cdd9e56f42c80150f5",
+    "url": "03398241db64f386dc27bb8812419a9c0b329318/static/js/main.5b3b1b05.chunk.js"
+  },
+  {
+    "revision": "28d670d4a273cfba37de",
+    "url": "03398241db64f386dc27bb8812419a9c0b329318/static/js/runtime-main.98937af5.js"
+  }
+]);
